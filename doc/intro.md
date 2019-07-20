adb58be6024293ce535c8aaa5825011fb5e8649e
Christopher Dumas <christopherdumas@gmail.com>
2019-07-19 19:19:38 -0700

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to daoleth
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/daoleth/core_test.clj
@@ -0,0 +1,7 @@
+(ns daoleth.core-test
+  (:require [clojure.test :refer :all]
+            [daoleth.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
